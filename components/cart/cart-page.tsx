c6795155e1d143a17b32cbb597eca1cec5ae17db
tiendx161202 <dangxuantien161202@gmail.com>
2024-05-07 19:06:07 +0700

    Cart_UI and Order_Detail_UI

--- components/cart/cart-page.tsx
+++ components/cart/cart-page.tsx
@@ -1,15 +1,14 @@
-import { Suspense } from 'react';
-
 import { ProductCartSchema, VariationDto } from '@techcell/node-sdk';
 import { productApiRequest } from '@/apiRequests/product';
 
 import { ProductCart } from '@/types/cart.type';
 
 import ListProductCart from './list-product-cart';
+import { Button } from '../ui/button';
+import AddressCart from './address-cart';
+import Image from 'next/image';
 import CartSuggest from './cart-suggest';
-import { BackButton } from '@/components/common/button-back';
-import { Separator } from '@/components/ui/separator';
-import { CartProductSkeleton } from './cart-product-skeleton';
+import ButtonCart from './button-cart';
 
 export async function getProductVariation(
   id: string,
@@ -45,23 +44,31 @@ export default async function CartPage({ cartProducts }: Readonly<CartPageProps>
   const cartProductsDetail = await Promise.all(promises);
 
   return (
-
-    <div className="">
-      <div className="px-5 sm:container sm:max-w-[640px] lg:max-w-[768px]">
-        <div className="w-full flex flex-col mt-5">
-          <div className="w-full relative h-8 sm:h-10 flex items-center my-2 ">
-            <div className="hidden sm:block">
-              <BackButton />
+    <div className="px-5 sm:container pb-4 h-auto">
+        <div className='w-full text-center my-4 text-lg'>Giỏ hàng của bạn</div>
+      <div className="flex flex-col sm:flex-row sm:justify-between md:justify-between lg:justify-between xl:justify-between 2xl:justify-between gap-5 w-full h-auto my-2">
+        <div className="w-full sm:w-2/3 md:w-2/3 lg:w-2/3 xl:w-2/3 h-auto ">
+          <div className="flex flex-col gap-4">
+            {/* danh sách sản phẩm */}
+            <div className="p-4 w-full bg-white rounded-md h-auto">
+              <ListProductCart products={cartProductsDetail} />
             </div>
-            <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl  sm:text-2xl font-semibold text-center">Giỏ hàng của bạn</p>
+
+            {/* chọn địa chỉ */}
+            <div className="p-4 w-full bg-white rounded-md h-auto">
+              <AddressCart />
+            </div>
+
+          {/* <CartSuggest /> */}
+
+
           </div>
-          <Separator className="border-black" />
-          <Suspense fallback={<CartProductSkeleton />}>
-            <ListProductCart products={cartProductsDetail} />
-          </Suspense>
+        </div>
+        <div className="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 h-[100px]  sticky top-4">
+          <ButtonCart  products={cartProductsDetail}/>
         </div>
       </div>
-      <CartSuggest />
+
     </div>
   );
 }
