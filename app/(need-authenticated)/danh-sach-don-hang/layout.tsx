d955a56775ce0c68db274a50ad04e7dcb29aca18
doancongvu <congvu021193@gmail.com>
2024-05-22 19:27:38 +0700

    comment before switch development

--- app/(need-authenticated)/danh-sach-don-hang/layout.tsx
+++ app/(need-authenticated)/danh-sach-don-hang/layout.tsx
@@ -1,8 +1,5 @@
 import MaxWidthWrapper from '@/components/common/max-width-wrapper';
-import OrderListProduct from '@/components/order/order-list-product';
 import SidebarOrder from '@/components/order/sidebar-order';
-import { NAV_ORDER } from '@/constants';
-import Link from 'next/link';
 import { Metadata } from 'next/types';
 
 export const metadata: Metadata = {
--- components/order/order-list-product.tsx
+++ components/order/order-list-product.tsx
@@ -1,18 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import { PHONE_TEST } from '@/constants/phone-test';
 import { ProductInListDto, ProductSchema } from '@techcell/node-sdk';
-
-import AlternativeImg from '@/public/phone-test/15-promax.jpg';
 import { currencyFormat } from '@/utilities/func.util';
-import { useEffect, useState } from 'react';
 import { productApiRequest } from '@/apiRequests';
 import { NormalCardSkeleton } from '../common/product-card/normal-card-skeleton';
 import Link from 'next/link';
 import TempProductImg from '@/public/phone-test/15-base.jpg';
 import { Separator } from '../ui/separator';
-import MaxWidthWrapper from '../common/max-width-wrapper';
 import { PhoneListOrder } from '@/constants/common';
 import { RootPath } from '@/constants';
 
@@ -95,57 +90,6 @@ const OrderListProduct = ({ product }: OrderListProductProps) => {
           </div>
         </div>
       </div>
-    // <div className="w-auto sm:w-[full] h-auto m-auto bg-white my-3 p-4 sm:p-0 sm:py-4">
-    //   <div className="">
-    //     <div className="text-center">
-    //       <div className="text-[20px] font-bold ml-2">Tất cả sản phẩm</div>
-    //     </div>
-    //     <div className="flex flex-col">
-    //       {products.map((phone) => (
-    //         <div key={phone.skuId} className="w-full h-full">
-    //           <div className="flex flex-row mb-1 pt-5 bg-white items-center rounded-xl">
-    //             <div className="w-[115px] h-[75px] sm:w-[100px] sm:h-[100px]">
-    //               <Image
-    //                 src={phone.image?.url ?? AlternativeImg.src}
-    //                 alt={phone.productName}
-    //                 width={200}
-    //                 height={200}
-    //                 style={{
-    //                   height: '100%',
-    //                   width: '100%',
-    //                   objectFit: 'cover',
-    //                   display: 'block',
-    //                 }}
-    //               />
-    //             </div>
-    //             <div className="flex flex-row justify-between w-full items-center">
-    //               <div className="flex flex-col ml-4">
-    //                 <span className="text-lg font-semiblod">{phone.productName}</span>
-    //                 <span>{phone.productType}</span>
-    //                 <span className="text-[#ee4949] text-lg font-semiblod">
-    //                   {currencyFormat(
-    //                     phone.unitPrice.special !== 0
-    //                       ? phone.unitPrice.special
-    //                       : phone.unitPrice.base,
-    //                   )}
-    //                   đ
-    //                   {phone.unitPrice.special !== 0 && (
-    //                     <span className="ml-2 text-slate-500 text-base line-through">
-    //                       {phone.unitPrice.base}đ
-    //                     </span>
-    //                   )}
-    //                 </span>
-    //               </div>
-    //               <div className="">
-    //                 <span className="text-xs sm:text-lg ">Số lượng : {phone.quantity}</span>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
